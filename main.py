5632b81c0d5bd2b3a5fcf9c0b68eb95a21d04ed9
Skeyling1 <skeyling1@gmail.com>
2024-09-28 20:04:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ screen = pygame.display.set_mode((WIDTH, HIGHT))
 pygame.display.set_caption("Snaky")
 
 #начальные положения персонажа
-x = 20
-y = 20
+x = 200
+y = 200
 direction_x = 0
 direction_y = 0
 
@@ -21,15 +21,18 @@ class BodyXsegment:
         self.body.fill((255, 255, 255))
         self.rect = self.body.get_rect()
     def moving(self):
-        for number in range(self.segment_number):
-            screen.blit(self.body, (x, y+number*2))
+        if self.segment_number == 0:
+            screen.blit(self.body, (x, y))
+        else:
+            screen.blit(self.body, (x, y))
 
 
 
 #создаем объект персонажа
-head = BodyXsegment(10, (x, y))
+head = BodyXsegment(0, (x, y))
+body1 = BodyXsegment(20, (x, y))
 
-
+coordinate = {}
 
 run = True
 while run:
@@ -40,6 +43,7 @@ while run:
     screen.fill((55, 55, 55))
 
     head.moving()
+    body1.moving()
 
 
     keys = pygame.key.get_pressed()
@@ -50,17 +54,31 @@ while run:
     if keys[pygame.K_RIGHT]:
         direction_x = 1
         direction_y = 0
+        coordinate = {"RIGHT" : (x, y)}
+
     elif keys[pygame.K_LEFT]:
         direction_x = -1
         direction_y = 0
+        coordinate = {"LEFT" : (x, y)}
+
     elif keys[pygame.K_UP]:
         direction_x = 0
         direction_y = -1
+        coordinate = {"UP" : (x, y)}
+
     elif keys[pygame.K_DOWN]:
         direction_x = 0
         direction_y = 1
+        coordinate = {"DOWN" : (x, y)}
+
+#проверка столкновения с стенками
 
 
+#проверка столкновения с едой
+
+
+
+    #print(coordinate)
     pygame.time.delay(10)
     pygame.display.flip()
 
